e756e40efd10f12412dadf89db3123b9c2ff061b
Holger Woltersdorf <hw@hollo.me>
2017-08-23 22:59:06 +0200

    Adds exception, if server has disconnected

---
+++ src/Exceptions/ServerDisconnectedException.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+/**
+ * @author hollodotme
+ */
+
+namespace PHPMQ\Client\Exceptions;
+
+/**
+ * Class ServerDisconnectedException
+ * @package PHPMQ\Client\Exceptions
+ */
+final class ServerDisconnectedException extends RuntimeException
+{
+
+}
