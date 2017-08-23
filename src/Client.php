f460ac28edcdb8cadc21231823cae9e2752b99af
Holger Woltersdorf <hw@hollo.me>
2017-08-23 23:00:11 +0200

    Adds registering message handlers, adds handling/reading messages, adds acknowledging messages, adds tests

--- src/Client.php
+++ src/Client.php
@@ -5,11 +5,23 @@
 
 namespace PHPMQ\Client;
 
+use PHPMQ\Client\Builders\MessageBuilder;
+use PHPMQ\Client\Exceptions\ServerDisconnectedException;
 use PHPMQ\Client\Sockets\Interfaces\EstablishesStream;
+use PHPMQ\Protocol\Constants\PacketLength;
+use PHPMQ\Protocol\Interfaces\BuildsMessages;
+use PHPMQ\Protocol\Interfaces\DefinesMessage;
+use PHPMQ\Protocol\Interfaces\IdentifiesMessage;
 use PHPMQ\Protocol\Interfaces\IdentifiesQueue;
 use PHPMQ\Protocol\Interfaces\ProvidesMessageData;
+use PHPMQ\Protocol\Messages\Acknowledgement;
+use PHPMQ\Protocol\Messages\ConsumeRequest;
 use PHPMQ\Protocol\Messages\MessageClientToServer;
+use PHPMQ\Protocol\Messages\MessageServerToClient;
+use PHPMQ\Protocol\Types\MessageHeader;
+use PHPMQ\Protocol\Types\PacketHeader;
 use PHPMQ\Stream\Constants\ChunkSize;
+use PHPMQ\Stream\Interfaces\TransfersData;
 
 /**
  * Class Client
@@ -17,26 +29,82 @@ use PHPMQ\Stream\Constants\ChunkSize;
  */
 final class Client
 {
+	private const STREAM_SELECT_TIMEOUT_USEC = 200000;
+
 	/** @var EstablishesStream */
 	private $clientSocket;
 
+	/** @var array|callable[] */
+	private $messageHandlers;
+
+	/** @var BuildsMessages */
+	private $messageBuilder;
+
+	/** @var bool */
+	private $handlingStarted = false;
+
 	public function __construct( EstablishesStream $clientSocket )
 	{
-		$this->clientSocket = $clientSocket;
+		$this->clientSocket    = $clientSocket;
+		$this->messageHandlers = [];
+		$this->messageBuilder  = new MessageBuilder();
 	}
 
 	public function sendMessage( IdentifiesQueue $queueName, string $content ) : void
 	{
 		$message = new MessageClientToServer( $queueName, $content );
 
-		$this->writeMessage( $message );
+		$this->sendToServer( $message );
 	}
 
-	private function writeMessage( ProvidesMessageData $message ) : void
+	private function sendToServer( ProvidesMessageData $message ) : void
 	{
 		$stream = $this->clientSocket->getStream();
 
 		$stream->writeChunked( $message->toString(), ChunkSize::WRITE );
+	}
+
+	public function requestMessages( IdentifiesQueue $queueName, int $amount ) : void
+	{
+		$consumeRequest = new ConsumeRequest( $queueName, $amount );
+
+		$this->sendToServer( $consumeRequest );
+	}
+
+	public function registerMessageHandlers( callable ...$messageHandlers ) : void
+	{
+		$this->messageHandlers = array_merge( $this->messageHandlers, $messageHandlers );
+	}
+
+	public function handleMessages() : void
+	{
+		$this->registerSignalHandler();
+
+		$this->handlingStarted = true;
+
+		while ( $this->handlingStarted )
+		{
+			$messages = $this->readMessages();
+
+			$this->processMessages( $messages );
+		}
+	}
+
+	private function registerSignalHandler() : void
+	{
+		if ( function_exists( 'pcntl_signal' ) )
+		{
+			pcntl_signal( SIGTERM, [$this, 'shutDownBySignal'] );
+			pcntl_signal( SIGINT, [$this, 'shutDownBySignal'] );
+		}
+	}
+
+	public function shutDownBySignal( int $signal ) : void
+	{
+		if ( in_array( $signal, [SIGINT, SIGTERM, SIGKILL], true ) )
+		{
+			$this->disconnect();
+		}
 	}
 
 	public function disconnect() : void
@@ -44,5 +112,110 @@ final class Client
 		$stream = $this->clientSocket->getStream();
 		$stream->shutDown();
 		$stream->close();
+	}
+
+	public function readMessages() : \Generator
+	{
+		$stream = $this->clientSocket->getStream();
+
+		if ( !$this->isStreamActive( $stream ) )
+		{
+			return;
+		}
+
+		do
+		{
+			$headerBytes = $stream->read( PacketLength::MESSAGE_HEADER );
+
+			$this->guardReadBytes( $headerBytes );
+
+			$messageHeader = MessageHeader::fromString( $headerBytes );
+
+			yield $this->readMessage( $messageHeader, $stream );
+		}
+		while ( $stream->hasUnreadBytes() );
+	}
+
+	private function isStreamActive( TransfersData $stream ) : bool
+	{
+		$readStreams  = [];
+		$writeStreams = null;
+		$except       = null;
+
+		$stream->collectRawStream( $readStreams );
+
+		usleep( self::STREAM_SELECT_TIMEOUT_USEC );
+
+		$active = @stream_select( $readStreams, $writeStreams, $except, 0, self::STREAM_SELECT_TIMEOUT_USEC );
+
+		return (bool)$active;
+	}
+
+	/**
+	 * @param bool|null|int $bytes
+	 *
+	 * @throws \PHPMQ\Client\Exceptions\ServerDisconnectedException
+	 */
+	private function guardReadBytes( $bytes ) : void
+	{
+		if ( !$bytes )
+		{
+			throw new ServerDisconnectedException( 'Message queue server disconnected.' );
+		}
+	}
+
+	/**
+	 * @param DefinesMessage $messageHeader
+	 * @param TransfersData  $stream
+	 *
+	 * @throws \PHPMQ\Client\Exceptions\ServerDisconnectedException
+	 * @throws \PHPMQ\Stream\Exceptions\ReadTimedOutException
+	 * @return ProvidesMessageData
+	 */
+	private function readMessage( DefinesMessage $messageHeader, TransfersData $stream ) : ProvidesMessageData
+	{
+		$packetCount = $messageHeader->getMessageType()->getPacketCount();
+		$packets     = [];
+		for ( $i = 0; $i < $packetCount; $i++ )
+		{
+			$bytes        = $stream->readChunked(
+				PacketLength::PACKET_HEADER,
+				ChunkSize::READ
+			);
+			$packetHeader = PacketHeader::fromString( $bytes );
+			$bytes        = $stream->readChunked( $packetHeader->getContentLength(), ChunkSize::READ );
+
+			$packets[ $packetHeader->getPacketType() ] = $bytes;
+		}
+
+		return $this->messageBuilder->buildMessage( $messageHeader, $packets );
+	}
+
+	private function processMessages( iterable $messages ) : void
+	{
+		foreach ( $messages as $message )
+		{
+			$this->notifyMessageHandlers( $message );
+		}
+	}
+
+	private function notifyMessageHandlers( MessageServerToClient $message ) : void
+	{
+		foreach ( $this->messageHandlers as $messageHandler )
+		{
+			$messageHandler( $message, $this );
+		}
+	}
+
+	public function acknowledgeMessage( IdentifiesQueue $queueName, IdentifiesMessage $messageId ) : void
+	{
+		$acknowledgement = new Acknowledgement( $queueName, $messageId );
+
+		$this->sendToServer( $acknowledgement );
+	}
+
+	public function stopHandlingMessages() : void
+	{
+		$this->handlingStarted = false;
 	}
 }
--- tests/Unit/ClientTest.php
+++ tests/Unit/ClientTest.php
@@ -8,8 +8,11 @@ namespace PHPMQ\Client\Tests\Unit;
 use PHPMQ\Client\Client;
 use PHPMQ\Client\Sockets\ClientSocket;
 use PHPMQ\Client\Sockets\Types\NetworkSocket;
+use PHPMQ\Client\Tests\Unit\Fixtures\Traits\MessageIdentifierMocking;
 use PHPMQ\Client\Tests\Unit\Fixtures\Traits\QueueIdentifierMocking;
 use PHPMQ\Client\Tests\Unit\Fixtures\Traits\SocketMocking;
+use PHPMQ\Protocol\Messages\MessageServerToClient;
+use PHPMQ\Stream\Constants\ChunkSize;
 use PHPMQ\Stream\Stream;
 use PHPUnit\Framework\TestCase;
 
@@ -21,6 +24,7 @@ final class ClientTest extends TestCase
 {
 	use SocketMocking;
 	use QueueIdentifierMocking;
+	use MessageIdentifierMocking;
 
 	protected function setUp() : void
 	{
@@ -38,12 +42,105 @@ final class ClientTest extends TestCase
 		$clientSocket    = new ClientSocket( new NetworkSocket( self::$SERVER_HOST, self::$SERVER_PORT ) );
 		$client          = new Client( $clientSocket );
 		$queueName       = $this->getQueueName( 'Unit-Test-Queue' );
-		$expectedMessage = 'H0100102P0100000000000000000000000000015'
-		                   . 'Unit-Test-Queue'
-		                   . 'P0200000000000000000000000000009'
-		                   . 'Unit-Test';
+		$expectedMessage = 'H0100102'
+			. 'P0100000000000000000000000000015'
+			. 'Unit-Test-Queue'
+			. 'P0200000000000000000000000000009'
+			. 'Unit-Test';
 
 		$client->sendMessage( $queueName, 'Unit-Test' );
+
+		$serverClientStream = $serverStream->acceptConnection();
+
+		$message = $serverClientStream->read( 1024 );
+
+		$this->assertSame( $expectedMessage, $message );
+
+		$serverClientStream->close();
+		$client->disconnect();
+	}
+
+	public function testCanRequestMessagesFromServer() : void
+	{
+		$serverStream    = new Stream( $this->serverSocket );
+		$clientSocket    = new ClientSocket( new NetworkSocket( self::$SERVER_HOST, self::$SERVER_PORT ) );
+		$client          = new Client( $clientSocket );
+		$queueName       = $this->getQueueName( 'Unit-Test-Queue' );
+		$expectedMessage = 'H0100202'
+			. 'P0100000000000000000000000000015'
+			. 'Unit-Test-Queue'
+			. 'P0400000000000000000000000000001'
+			. '5';
+
+		$client->requestMessages( $queueName, 5 );
+
+		$serverClientStream = $serverStream->acceptConnection();
+
+		$message = $serverClientStream->read( 1024 );
+
+		$this->assertSame( $expectedMessage, $message );
+
+		$serverClientStream->close();
+		$client->disconnect();
+	}
+
+	public function testCanHandleMessagesByCallback() : void
+	{
+		$serverStream = new Stream( $this->serverSocket );
+		$clientSocket = new ClientSocket( new NetworkSocket( self::$SERVER_HOST, self::$SERVER_PORT ) );
+		$client       = new Client( $clientSocket );
+		$queueName    = $this->getQueueName( 'Unit-Test-Queue' );
+		$messageId    = $this->getMessageId( 'Unit-Test-ID' );
+		$message      = new MessageServerToClient(
+			$messageId,
+			$queueName,
+			'Hello World'
+		);
+
+		$expectedMessage = 'H0100303'
+			. 'P0100000000000000000000000000015'
+			. 'Unit-Test-Queue'
+			. 'P0200000000000000000000000000011'
+			. 'Hello World'
+			. 'P0300000000000000000000000000012'
+			. 'Unit-Test-ID';
+
+		$client->registerMessageHandlers(
+			function ( MessageServerToClient $message, Client $client )
+			{
+				echo $message->toString();
+
+				$client->stopHandlingMessages();
+			}
+		);
+
+		$client->requestMessages( $queueName, 1 );
+
+		$serverClientStream = $serverStream->acceptConnection();
+		$serverClientStream->writeChunked( $message->toString(), ChunkSize::WRITE );
+
+		$client->handleMessages();
+
+		$this->expectOutputString( $expectedMessage );
+
+		$serverClientStream->close();
+		$client->disconnect();
+	}
+
+	public function testCanAcknowledgeMessages() : void
+	{
+		$serverStream    = new Stream( $this->serverSocket );
+		$clientSocket    = new ClientSocket( new NetworkSocket( self::$SERVER_HOST, self::$SERVER_PORT ) );
+		$client          = new Client( $clientSocket );
+		$queueName       = $this->getQueueName( 'Unit-Test-Queue' );
+		$messageId       = $this->getMessageId( 'Unit-Test-ID' );
+		$expectedMessage = 'H0100402'
+			. 'P0100000000000000000000000000015'
+			. 'Unit-Test-Queue'
+			. 'P0300000000000000000000000000012'
+			. 'Unit-Test-ID';
+
+		$client->acknowledgeMessage( $queueName, $messageId );
 
 		$serverClientStream = $serverStream->acceptConnection();
 
